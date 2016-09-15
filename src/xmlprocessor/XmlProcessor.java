9c0cec6c5c270694a7833cd54dedb5d3bd96fd94
oscarb <a257399@uach.mx>
2016-09-15 13:13:55 -0600

    Primer commit

---
+++ src/xmlprocessor/XmlProcessor.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xmlprocessor;
+//import org.jdom2.Document;
+//import org.jdom2.Element;
+//import org.jdom2.Attribute;
+//import org.jdom2.output.Format;
+//import org.jdom2.output.XMLOutputter;
+//import org.jdom2.input.SAXBuilder;
+import org.jdom2.*;
+import org.jdom2.output.*;
+import org.jdom2.input.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.util.List;
+
+
+
+/**
+ *
+ * @author oscar
+ */
+public class XmlProcessor {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     
+       
+ }
+    
+}
